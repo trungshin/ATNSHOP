7143be5a11fda3e9bd9836455686cae12e2094ec
trungshin <41856824+trungshin@users.noreply.github.com>
2020-12-22 18:36:26 +0700

    Update config_DB.js

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -7,7 +7,7 @@ module.exports = {
     },
 
     clouddb: {
-        urldb : 'mongodb://localhost:27017/shopsmall',
+        urldb : 'mongodb+srv://trungshin_6969:QK3xrnkUn8MUUBUw@cluster0.krj6n.mongodb.net/shopsmall?retryWrites=true&w=majority',
         dbname : "shopsmall",
         dbusername : "",
         dbpassword : "",
